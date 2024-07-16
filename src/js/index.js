933d541a95dfe690d8a815c4b54d9004e9275c67
caamomilaa <c.velazquezdamico@gmail.com>
2024-07-16 18:16:39 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,12 @@ const ALL_WORDS = ["casa", "coche", "ornitorrinco", "velero"];
 const NUMBER_OF_TRIES = 5;
 
 const gameBoardElement = document.getElementById("game-board");
+const userWordFormElement = document.getElementById("user-word-form");
 const modalElement = document.getElementById("modal");
 const popUpElement = document.getElementById("pop-up");
+
+let secretWord;
+let currentRow = 0;
 
 const chooseSecretWord = () => {
   const randomNumber = Math.floor(Math.random() * ALL_WORDS.length);
@@ -18,7 +22,51 @@ const createGameBoard = () => {
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < NUMBER_OF_TRIES; i++) {
     const newRow = document.createElement("div");
+    newRow.classList.add("game-board__row");
+    for (let j = 0; j < secretWord.length; j++) {
+      const newLetterContainer = document.getElementById("span");
+      newLetterContainer.classList.add("letter");
+      newRow.append(newLetterContainer);
+    }
+    fragment.append(newRow);
   }
+  gameBoardElement.append(fragment);
 };
 
+const printWord = (userWord) => {
+  let wordToCheck = secretWord;
+
+  for (let i = 0; i < userWord.length; i++) {
+    const letter = userWord[i];
+    const letterContainer = gameBoardElement.children[currentRow].children[i];
+    letterContainer.textContent = letter;
+
+    if (letter == secretWord[i]) {
+      letterContainer.classList.add("letter--correct");
+      wordToCheck = wordToCheck.replace(letter, "-");
+    }
+  }
+
+  for (let i = 0; i < userWord.length; i++) {
+    const letter = userWord[i];
+    const letterContainer = gameBoardElement.children[currentRow].children[i];
+    if (wordToCheck.includes(letter)) {
+      letterContainer.classList.add("letter-present");
+      wordToCheck = wordToCheck.replace(letter, "-");
+    } else {
+      if (!letterContainer.classList.contains("letter--correct"))
+        letterContainer.classList.add("letter--incorrect");
+    }
+  }
+
+  currentRow++;
+};
+
+userWordFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (!event.target.word.value) return;
+  printWord(event.target.word.value);
+});
+
 chooseSecretWord();
+createGameBoard();
